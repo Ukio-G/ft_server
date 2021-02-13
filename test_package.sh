cd8d366356947596b330049f1d540667ab1f50e5
Aracely Tawana <atawana@21-school.ru>
2021-02-13 17:43:31 +0000

    add apache to conflicts in contol; add php support out-of-the-box

--- test_package.sh
+++ test_package.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 ./create_package.sh
+
+apt-get remove nginx php php7.3-fpm php7.3-common mariadb-server php-mysql php-fpm nginx-common
 apt-get -y remove ft-server
-cp ft_server_package.deb /
+mv ft_server_package.deb /
 cd / && apt install ./ft_server_package.deb
 cd /root/ft_server
