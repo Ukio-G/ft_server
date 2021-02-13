2517947ce18a268d1f9fef0ee0f300516f555796
Aracely Tawana <atawana@21-school.ru>
2021-02-13 18:04:57 +0000

    create dir for php-fpm socket file

--- test_package.sh
+++ test_package.sh
@@ -2,8 +2,8 @@
 
 ./create_package.sh
 
-apt-get remove nginx php php7.3-fpm php7.3-common mariadb-server php-mysql php-fpm nginx-common
+apt-get -y remove nginx php php7.3-fpm php7.3-common mariadb-server php-mysql php-fpm nginx-common
 apt-get -y remove ft-server
 mv ft_server_package.deb /
-cd / && apt install ./ft_server_package.deb
+cd / && apt -y install ./ft_server_package.deb
 cd /root/ft_server
